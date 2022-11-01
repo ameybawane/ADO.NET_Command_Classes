8ebd03ddb8096a94ee555a2c718780b92679d0ef
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2022-11-01 17:15:27 +0530

    Add files via upload

---
+++ ADO_Methods_Ex/ADO_Methods_Ex/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ADO_Methods_Ex.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" ID="btnGetDevice" Height="30px" Text="Show Operations" OnClick="btnGetDevice_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+           <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADO_Methods_Ex/ADO_Methods_Ex/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO_Methods_Ex
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetDevice_Click(object sender, EventArgs e)
+        {
+            string db = ConfigurationManager.ConnectionStrings["dbSample"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(db))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                con.Open();
+
+                cmd.CommandText = "INSERT INTO tblDevice VALUES('OnePlus', 'OP 9R', 59000)";
+                int rowsInserted = cmd.ExecuteNonQuery();
+                Label1.Text = "Rows Inserted : " + rowsInserted.ToString();
+
+                cmd.CommandText = "UPDATE tblDevice SET DevicePrice = 78999 WHERE DeviceID = 5";
+                int rowsUpdated = cmd.ExecuteNonQuery();
+                Label2.Text = "Rows Updated : " + rowsUpdated.ToString();
+
+                cmd.CommandText = "DELETE FROM tblDevice WHERE DeviceID = 9";
+                int rowsDeleted = cmd.ExecuteNonQuery();
+                Label3.Text = "Rows Deleted : " + rowsDeleted.ToString();
+
+            }
+        }
+    }
+}
